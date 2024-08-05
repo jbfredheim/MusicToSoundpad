6a65b7cea096ca019e2ad71b42025435241be034
Joachim Birkelund Fredheim <joachim@auge-tech.no>
2024-08-05 15:26:50 +0200

    add youtube and tidal support

--- src/main.c
+++ src/main.c
@@ -5,8 +5,11 @@
 #include <Urlmon.h>
 #include "md5.h"
 #include "pipes.h"
+#include <wctype.h>
 
 #pragma comment(lib, "urlmon.lib")
+
+static wchar_t lastWindowTitle[256] = L"";
 
 void CheckAndCreateDirectory(const wchar_t* directoryName) {
     // Check if the directory exists
@@ -70,16 +73,34 @@ DWORD GetProcessIdByName(const wchar_t* processName) {
     return pid; // Return the process ID, this will remain as 0 if not found
 }
 
-void SanitiseTitle(wchar_t *title) {
-    wchar_t invalidCharacters[] = { '<', '>', ':', '"', '/', '\\', '|', '?', '*', '&'};
-    for (int i = 0; i < wcslen(title); i++) {
-        for (int j = 0; j < sizeof(invalidCharacters); j++) {
+void RemoveSubstring(wchar_t* str, const wchar_t* toRemove) {
+    wchar_t* pos;
+    size_t len = wcslen(toRemove);
+    while ((pos = wcsstr(str, toRemove)) != NULL) {
+        // Shift the remainder of the string left to overwrite the substring
+        wmemmove(pos, pos + len, wcslen(pos + len) + 1);
+    }
+}
+
+void SanitiseTitle(wchar_t* title) {
+    wchar_t invalidCharacters[] = { L'<', L'>', L':', L'"', L'/', L'\\', L'|', L'?', L'*', L'&' };
+    int len = wcslen(title);
+    for (int i = 0; i < len; i++) {
+        // Check for invalid characters and replace with '_'
+        for (int j = 0; j < sizeof(invalidCharacters) / sizeof(invalidCharacters[0]); j++) {
             if (title[i] == invalidCharacters[j]) {
-                title[i] = '_';
+                title[i] = L'_';
                 break;
             }
         }
+        // Check for control characters and replace with '_'
+        if (iswcntrl(title[i])) {
+            title[i] = L'_';
+        }
     }
+
+    // remove ' - Youtube - Google Chrome' suffic if present to give more predictable search results on youtube
+    RemoveSubstring(title, L" - YouTube - Google Chrome");
 }
 
 BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
@@ -93,79 +114,108 @@ BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam) {
             if (wcscmp(windowTitle, L"Spotify") != 0
                 && wcscmp(windowTitle, L"Spotify Premium") != 0
                 && wcscmp(windowTitle, L"Spotify Free") != 0
-                && wcscmp(windowTitle, L"GDI+ Window (Spotify.exe)") != 0) {
-                wprintf(L"Now Playing: %s\n", windowTitle);
+                && wcscmp(windowTitle, L"GDI+ Window (Spotify.exe)") != 0
+                && wcscmp(windowTitle, L"TIDAL") != 0 )
+            {
 
-                // If the title changes, send a pause command to the process
-                HWND hwndSpotify = FindWindowW(NULL, windowTitle);
-                if (hwndSpotify != NULL) {
-                    SendMessageW(hwndSpotify, WM_APPCOMMAND, 0, MAKELPARAM(0, APPCOMMAND_MEDIA_PAUSE));
+                if (wcsstr(windowTitle, L"Default IME") || wcsstr(windowTitle, L"MSCTFIME")) {
+                    return TRUE;
+                }
+                
+                // Check for presence of substring 'Google Chrome' and 'YouTube', require both to be present in windowtitle, skip if not present
+                if (wcsstr(windowTitle, L"Google Chrome") && !wcsstr(windowTitle, L"YouTube")) {
                     
-                    // Sanitise the title, some characters aren't allowed in file names
-                    SanitiseTitle(windowTitle);
+                    return TRUE;
+                }
 
-                    // Execute yt-dlp.exe
-                    DWORD lastError;
-                    STARTUPINFOW si = { sizeof(si) };
-                    PROCESS_INFORMATION pi;
-                    wchar_t commandLine[MAX_PATH];
-                    swprintf_s(commandLine, MAX_PATH, L"\"utils\\yt-dlp.exe\" -f ba[ext=m4a] -o \"%%USERPROFILE%%\\Music\\%s.%%(ext)s\" ytsearch:\"%s\"", windowTitle, windowTitle);
-                    if (CreateProcessW(NULL, commandLine, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
-                        // Wait for the process to finish before continuing
-                        WaitForSingleObject(pi.hProcess, INFINITE);
+                // Check for youtube homepage, skip if present
+                if (
+                    !wcscmp(windowTitle, L"YouTube - Google Chrome") 
+                    || !wcscmp(windowTitle, L"Subscriptions - YouTube - Google Chrome")
+                    || !wcscmp(windowTitle, L"Library - YouTube - Google Chrome")
+                    ) {
+                    return TRUE;
+                }
 
-                        // FIXME: We need to convert windowTitle from Multibyte to Byte
+                if (wcscmp(windowTitle, lastWindowTitle) != 0) {
+                    wprintf(L"Now Playing: %s\n", windowTitle);
+                    wcscpy_s(lastWindowTitle, sizeof(lastWindowTitle) / sizeof(lastWindowTitle[0]), windowTitle);
 
-                        // Determine the size of the required buffer
-                        size_t bufferSize = 0;
-                        if (wcstombs_s(&bufferSize, NULL, 0, windowTitle, 0) != 0) {
-                            perror("wcstombs_s");
-                            return 1;
-                        }
+                    // If the title changes, send a pause command to the process
+                    HWND hwndSpotify = FindWindowW(NULL, windowTitle);
+                    if (hwndSpotify != NULL) {
+                        SendMessageW(hwndSpotify, WM_APPCOMMAND, 0, MAKELPARAM(0, APPCOMMAND_MEDIA_PAUSE));
 
-                        // Allocate memory for the narrow character buffer
-                        char* narrowWindowTitle = (char*)malloc(bufferSize + 1); // +1 for null terminator
-                        if (narrowWindowTitle == NULL) {
-                            perror("malloc");
-                            return 1;
-                        }
+                        // Sanitise the title, some characters aren't allowed in file names
+                        SanitiseTitle(windowTitle);
 
-                        // Perform the conversion
-                        if (wcstombs_s(NULL, narrowWindowTitle, bufferSize + 1, windowTitle, bufferSize) != 0) {
-                            perror("wcstombs");
-                            free(narrowWindowTitle);
-                            return 1;
-                        }
 
-                        // We now have to construct a path to the .m4a file for SoundPad
-                        // Get the value of the USERPROFILE environment variable
-                        char* userProfileEnv = getenv("USERPROFILE");
-                        if (userProfileEnv == NULL) {
-                            fprintf(stderr, "USERPROFILE environment variable not found\n");
-                            return 1;
-                        }
+                        // Execute yt-dlp.exe
+                        DWORD lastError;
+                        STARTUPINFOW si = { sizeof(si) };
+                        PROCESS_INFORMATION pi;
+                        wchar_t commandLine[512];
+                        swprintf_s(commandLine, 512, L"\"utils\\yt-dlp.exe\" -f ba[ext=m4a] -o \"%%USERPROFILE%%\\Music\\%s.%%(ext)s\" ytsearch:\"%s\"", windowTitle, windowTitle);
 
-                        // Construct the path using USERPROFILE
-                        char path[512];
-                        snprintf(path, sizeof(path), "%s\\Music\\%s.m4a", userProfileEnv, narrowWindowTitle);
+                        if (CreateProcessW(NULL, commandLine, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+                            // Wait for the process to finish before continuing
+                            WaitForSingleObject(pi.hProcess, INFINITE);
 
-                        // Add to SoundPad library and start playing
-                        checkForEntry(getSoundList(), path);
+                            // Determine the size of the required buffer
 
-                        CloseHandle(pi.hProcess);
-                        CloseHandle(pi.hThread);
-                    }
-                    else {
-                        lastError = GetLastError();
-                        LPWSTR errorMessage = NULL;
-                        FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-                            NULL, lastError, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPWSTR)&errorMessage, 0, NULL);
-                        if (errorMessage != NULL) {
-                            wprintf(L"Failed to execute yt-dlp.exe. Error code: %lu, Error message: %s\n", lastError, errorMessage);
-                            LocalFree(errorMessage);
+                            size_t bufferSize = 0;
+                            if (wcstombs_s(&bufferSize, NULL, 0, windowTitle, 0) != 0) {
+                                perror("wcstombs_s");
+                                wprintf("In testing I found that errors here might stem from invalid characters in youtube video titles. Try another video\n");
+                                return 1;
+                            }
+
+                            // Allocate memory for the narrow character buffer
+                            char* narrowWindowTitle = (char*)malloc(bufferSize + 1); // +1 for null terminator
+                            if (narrowWindowTitle == NULL) {
+                                perror("malloc");
+                                return 1;
+                            }
+
+                            // Perform the conversion
+
+                            if (wcstombs_s(NULL, narrowWindowTitle, bufferSize + 1, windowTitle, bufferSize) != 0) {
+                                perror("wcstombs");
+                                free(narrowWindowTitle);
+                                return 1;
+                            }
+
+                            // We now have to construct a path to the .m4a file for SoundPad
+                            // Get the value of the USERPROFILE environment variable
+                            char* userProfileEnv = getenv("USERPROFILE");
+                            if (userProfileEnv == NULL) {
+                                fprintf(stderr, "USERPROFILE environment variable not found\n");
+                                return 1;
+                            }
+
+                            // Construct the path using USERPROFILE
+
+                            char path[512];
+                            snprintf(path, sizeof(path), "%s\\Music\\%s.m4a", userProfileEnv, narrowWindowTitle);
+
+                            // Add to SoundPad library and start playing
+                            checkForEntry(getSoundList(), path);
+
+                            CloseHandle(pi.hProcess);
+                            CloseHandle(pi.hThread);
                         }
                         else {
-                            wprintf(L"Failed to execute yt-dlp.exe. Error code: %lu\n", lastError);
+                            lastError = GetLastError();
+                            LPWSTR errorMessage = NULL;
+                            FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                                NULL, lastError, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPWSTR)&errorMessage, 0, NULL);
+                            if (errorMessage != NULL) {
+                                wprintf(L"Failed to execute yt-dlp.exe. Error code: %lu, Error message: %s\n", lastError, errorMessage);
+                                LocalFree(errorMessage);
+                            }
+                            else {
+                                wprintf(L"Failed to execute yt-dlp.exe. Error code: %lu\n", lastError);
+                            }
                         }
                     }
                 }
@@ -180,15 +230,52 @@ void GetMainWindowTitle(DWORD processId) {
     EnumWindows(EnumWindowsProc, (LPARAM)&processId);
 }
 
+int GetUserChoice() {
+    wchar_t input[3];
+    int choice = 2; // Default choice
+
+    wprintf(L"\nHello! Welcome to my weird modded version of MusicToSoundpad.\nBeware that whilst the google chrome 'scanner' does work, it is not as good as the spotify/tidal versions.\nHmu. on discord @joachimfr with any questions\nOriginal version by L-uu can be found here: https://github.com/L-uu/MusicToSoundpad\n\n");
+
+    wprintf(L"Select the process to monitor:\n");
+    wprintf(L"1. chrome.exe\n");
+    wprintf(L"2. spotify.exe (default)\n");
+    wprintf(L"3. tidal.exe\n");
+    wprintf(L"Enter your choice (1, 2, or 3, press Enter for default): ");
+
+    if (fgetws(input, sizeof(input) / sizeof(input[0]), stdin) != NULL) {
+        // Remove newline character if present
+        input[wcslen(input) - 1] = L'\0';
+
+        if (wcslen(input) > 0) {
+            int parsedChoice;
+            if (swscanf_s(input, L"%d", &parsedChoice) == 1) {
+                if (parsedChoice == 1 || parsedChoice == 2 || parsedChoice == 3) {
+                    choice = parsedChoice;
+                }
+                else {
+                    wprintf(L"Invalid choice. Exiting.\n");
+                    return -1;
+                }
+            }
+            else {
+                wprintf(L"Invalid input. Exiting.\n");
+                return -1;
+            }
+        }
+    }
+
+    return choice;
+}
+
 int main() {
     // Check and create the "utils" directory
     const wchar_t* utilsDirectoryName = L"utils";
     CheckAndCreateDirectory(utilsDirectoryName);
 
     // Check for updates
-    if (!CheckUpdates(L"MusicToSoundpad.exe", L"http://96.126.111.48/MusicToSoundpad/MusicToSoundpad.exe")) {
-        return 1; // Something has gone badly wrong
-    }
+    //if (!CheckUpdates(L"MusicToSoundpad.exe", L"http://96.126.111.48/MusicToSoundpad/MusicToSoundpad.exe")) {
+      //  return 1; // Something has gone badly wrong
+    //}
     if (!CheckUpdates(L"utils\\ffmpeg.exe", L"http://96.126.111.48/MusicToSoundpad/ffmpeg.exe")) {
         return 1; // Exit if failed to download ffmpeg.exe
     }
@@ -196,7 +283,28 @@ int main() {
         return 1; // Exit if failed to download yt-dlp.exe
     }
 
-    const wchar_t* processName = L"Spotify.exe";
+    const wchar_t* processName;
+
+    int choice = GetUserChoice();
+    if (choice == -1) {
+        return 1; // Exit if invalid input
+    }
+
+    switch (choice) {
+    case 1:
+        processName = L"chrome.exe";
+        break;
+    case 2:
+        processName = L"Spotify.exe";
+        break;
+    case 3:
+        processName = L"TIDAL.exe";
+        break;
+    default:
+        wprintf(L"Invalid choice. Exiting.\n");
+        return 1;
+    }
+
     DWORD pid;
 
     // We don't want to spam the console
@@ -215,6 +323,7 @@ int main() {
         } else {
             if (!isMessagePrinted2) {
                 wprintf(L"Process %s is not running.\n", processName);
+                wprintf(L"Restart the application to change your source choice\n");
                 isMessagePrinted1 = FALSE;
                 isMessagePrinted2 = TRUE;
             }
